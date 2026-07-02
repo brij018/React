2719107b65246bac752013ef2db928b1d29d241a
Brij Rathod <br1jrathod211@gmail.com>
2026-07-02 15:16:15 +0530

    to-do

---
+++ First_Project/to-do/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo_01 from "./components/Todo_01";
+
+const App = () => {
+  return (
+    <>
+      <Todo_01 />
+    </>
+  );
+};
+
+export default App;
---
+++ First_Project/to-do/src/components/Todo_01.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import { useState } from "react";
+
+const Todo_01 = () => {
+  const [input, setInput] = useState({
+    id: "",
+    task: "",
+    description: "",
+    status: "pending",
+  });
+  const [list, setList] = useState([]);
+  const [count, setCount] = useState(1);
+
+  const [editInput, setEditInput] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTask = {
+      ...input,
+      id: count,
+    };
+
+    setList((prev) => [...prev, newTask]);
+
+    setCount((prev) => prev + 1);
+
+    setInput({
+      id: "",
+      task: "",
+      description: "",
+      status: "pending",
+    });
+  };
+  const markDone = (id) => {
+    setList(
+      list.map((item) =>
+        item.id === id ? { ...item, status: "completed" } : item,
+      ),
+    );
+  };
+
+  const deleteTask = (index) => {
+    setList(list.filter((_, i) => i !== index));
+  };
+
+  const saveTask = (e) => {
+    e.preventDefault();
+    setList(list.map((task) => (task.id === editInput.id ? editInput : task)));
+    setEditInput(null);
+  };
+  return (
+    <>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          value={input.task}
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="description"
+          value={input.description}
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Task</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Done</th>
+            <th>Delete</th>
+            <th>Update</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((task, index) => (
+            <tr key={task.id}>
+              <td>{index + 1}</td>
+              <td>{task.task}</td>
+              <td>{task.description}</td>
+              <td>{task.status}</td>
+              <td>
+                <button onClick={() => markDone(task.id)}>Done</button>
+              </td>
+              <td>
+                <button onClick={() => deleteTask(index)}>Delete</button>
+              </td>
+              <td>
+                <button onClick={() => setEditInput(task)}>Update</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {editInput && (
+        <form onSubmit={saveTask}>
+          <input
+            type="text"
+            value={editInput.task}
+            onChange={(e) => {
+              setEditInput({ ...editInput, task: e.target.value });
+            }}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            value={editInput.description}
+            onChange={(e) => {
+              setEditInput({ ...editInput, description: e.target.value });
+            }}
+          />
+          <br />
+          <br />
+          <button type="submit">Save</button>
+        </form>
+      )}
+    </>
+  );
+};
+
+export default Todo_01;
